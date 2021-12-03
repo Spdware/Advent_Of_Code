841013ce0f4e7537d6a722ba1378c156d151a163
Spdware <70917350+Spdware@users.noreply.github.com>
2021-12-03 11:45:37 +0100

    Add files via upload

---
+++ AoC_Day_2.py
@@ -0,0 +1,47 @@
+depht = 0
+hor = 0
+
+
+def moving(depht,hor):
+    i = True
+    while True:
+        while True:
+            x = input('Insert the tipology of movement(\n1 : down \n2 : up \n 3: forward \n0 : exit\n)\n')
+            if (x == '1' or x == '2' or x == '3' or x == '0') :
+                break
+        
+        
+        if x != '0':
+
+            while True:
+                y = input('Insert distance:')
+
+                y = int(y)
+
+                if y >= 0:
+                    break
+
+            x = int(x)
+           
+            if x == 1:
+                depht += y   #down
+            elif x == 2:
+                 depht -= y   #up
+            elif x == 3:
+               hor += y     #forward
+           
+                
+            print(depht, hor,'\n')  
+        else:
+            break  
+
+        
+          
+moving(depht,hor)
+                
+
+
+       
+
+
+           
